fad3a37536ba9cc025eece14b2d7a8a2fbbf6931
akara <55230837+richeyphu@users.noreply.github.com>
2025-03-02 04:33:16 +0700

    chore: install svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,20 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-	prefetch: true,
-	image: {
-		service: passthroughImageService(),
-	},
-	integrations: [tailwind()],
-	vite: {
-		server: {
-			watch: {
-				ignored: ['./wordpress/**'],
-			},
-		},
-	},
+    prefetch: true,
+    image: {
+        service: passthroughImageService(),
+    },
+    integrations: [tailwind(), svelte()],
+    vite: {
+        server: {
+            watch: {
+                ignored: ['./wordpress/**'],
+            },
+        },
+    },
 });
