035feccc0b03fef9d4abba08c5f10ddc22ea87c6
akara <55230837+richeyphu@users.noreply.github.com>
2025-03-11 02:01:32 +0700

    refactor: move navigation data to separate file

---
+++ src/components/Nav.data.ts
@@ -0,0 +1,86 @@
+export const nav = [
+	{
+		name: 'หน้าแรก',
+		link: '/',
+	},
+	{
+		name: 'เกี่ยวกับวิทยาลัย',
+		link: '/about/history',
+		sub: [
+			{
+				name: 'ประวัติวิทยาลัย',
+				link: '/about/history',
+			},
+			{
+				name: 'วิสัยทัศน์ พันธกิจ ค่านิยม',
+				link: '/about/vision',
+			},
+			{
+				name: 'โครงสร้างองค์กร',
+				link: '/about/organization',
+			},
+			{
+				name: 'คณะผู้บริหาร',
+				link: '/about/executives',
+			},
+			{
+				name: 'บุคลากร',
+				link: '/about/personnel',
+			},
+			{
+				name: 'คณาจารย์',
+				link: '/about/teachers',
+			},
+		],
+	},
+	{
+		name: 'หลักสูตรที่เปิดสอน',
+		link: '/education/syllabus',
+		sub: [
+			{
+				name: 'หลักสูตร',
+				link: '/education/syllabus',
+			},
+			{
+				name: 'TCAS',
+				link: '/education/tcas',
+			},
+		],
+	},
+	{
+		name: 'นักศึกษาปัจจุบัน',
+		link: '/',
+	},
+	{
+		name: 'อาจารย์/เจ้าหน้าที่',
+		link: '/',
+		sub: [
+			{
+				name: 'สวัสดิการ',
+				link: '/',
+			},
+		],
+	},
+	{
+		name: 'ติดต่อเรา',
+		link: '/contact',
+		sub: [
+			{
+				name: 'ข้อมูลการติดต่อ',
+				link: '/contact',
+			},
+			{
+				name: 'ร้องเรียนทั่วไป',
+				link: '/contact/complaint',
+			},
+			{
+				name: 'ร้องเรียนการทุจริต',
+				link: '/contact/whistleblower',
+			},
+		],
+	},
+	{
+		name: 'ITA',
+		link: '/ita',
+	},
+];
