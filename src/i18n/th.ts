def2394d9261017ce469f28ed3294b80f368790e
sathapornjin-sketch <sathaporn.jin@mahidol.ac.th>
2026-01-09 16:06:10 +0700

    update9jan2026_4.05

---
+++ src/i18n/th.ts
@@ -0,0 +1,17 @@
+export default {
+  siteTitle: 'วิทยาลัยศาสนศึกษา – มหาวิทยาลัยมหิดล',
+  siteDescription: 'College of Religious Studies, Mahidol University',
+
+  collegeName: 'วิทยาลัยศาสนศึกษา มหาวิทยาลัยมหิดล',
+
+  history: 'ประวัติความเป็นมา',
+  vision: 'พันธกิจและวิสัยทัศน์',
+  foundation: 'มูลนิธิและหอพักน้ำทองสิกขาลัย',
+
+  courses: 'หลักสูตร',
+  news: 'ข่าวสาร CRS',
+  seminar: 'สัมมนาและบรรยายทางวิชาการ',
+  students: 'นักศึกษา CRS Students',
+  activities: 'กิจกรรม',
+  birth: 'ทำบุญเดือนเกิด',
+};
